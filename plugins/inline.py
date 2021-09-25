aea0d03c87badcba82728887255e1b4e6439bae4
Pumudu-railway <87109326+Pumudu-railway@users.noreply.github.com>
2021-09-25 14:25:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -22,8 +22,8 @@ from logger import LOGGER
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('📲Update Channel', url='https://t.me/Thekumikosetsuko'),
+        InlineKeyboardButton('💬Support chat', url='https://t.me/kumikosetsuko'),
     ]
     ]
 def get_cmd(dur):
@@ -38,7 +38,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
